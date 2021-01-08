2fb6b218663a6d21d80e44c0b3ca551d0669a616
Leander Harding <lsh@lsh.io>
2021-01-07 16:07:25 -0800

    WIP: planetInfo FFI wrapper

--- ephemeris/ios/Classes/ephemeris.c
+++ ephemeris/ios/Classes/ephemeris.c
@@ -15,96 +15,53 @@ void init() {
 }
 
 __attribute__((visibility("default"))) __attribute__((used))
-double ephemerisTime(int year, int month, int day, double hour) {
+double julianDay(int year, int month, int day, double hour) {
     double tjd_ut = swe_julday(year, month, day, hour, SE_GREG_CAL);
-    return tjd_ut + swe_deltat(tjd_ut);
+    return tjd_ut; // + swe_deltat(tjd_ut); not necessary, we're using swe_calc_ut
 }
 
-typedef struct planetResult {
+typedef struct {
+    double longitude;
+    double latitude;
+    double distance;
+} skyPoint;
+
+/**
+ * WARNING: THIS! IS! NOT! THREAD! SAFE!
+ * Because of how Dart does threading this should be okay as long as in the Dart
+ * code you copy your results out of the planetResult struct before calling planetInfo
+ * again. This *should* be handled in the Dart wrapper, but this comment is your last
+ * warning just in case that gets routed around somehow.
+ */
+typedef struct {
+    skyPoint position;
+    skyPoint speed;
     char serr[AS_MAXCH];
-}
+} planetResult_t;
+
+planetResult_t planetResult;
 
 __attribute__((visibility("default"))) __attribute__((used))
-void planetInfo(int year, int month, int day, int hour) {
-            iflgret = swe_calc_ut(tjd_ut, p, iflag, x2, serr);
+planetResult_t* planetInfo(int planet, double julianDay) {
+    double planetParams[6];
+    long iflag = SEFLG_SPEED;
+    long iflgret = swe_calc_ut(julianDay, planet, iflag, planetParams, planetResult.serr);
 
-             /* if there is a problem, a negative value is returned and an
-             * error message is in serr.
-             */
+     /* if there is a problem, a negative value is returned and an
+     * error message is in serr.
+     */
 
-            if (iflgret < 0)
-                 printf("error: %s\n", serr);
-}
+    if (iflgret < 0)
+         printf("error: %s\n", planetResult.serr);
 
-__attribute__((visibility("default"))) __attribute__((used))
-void planet_info(int year, int month, int day, int hour) {
-    char *sp, sdate[AS_MAXCH], snam[40], serr[AS_MAXCH];
-    int jday = 1, jmon = 1, jyear = 2000;
-    double jut = 0.0;
-    double tjd_ut, te, x2[6];
-    long iflag, iflgret;
-    int p;
+    // array of 6 doubles for longitude, latitude, distance, speed in long., speed in lat., and speed in dist.
+    int sigh = 0;
+    planetResult.position.longitude = planetParams[sigh];
+    planetResult.position.latitude = planetParams[sigh++];
+    planetResult.position.distance = planetParams[sigh++];
+    planetResult.speed.longitude = planetParams[sigh++];
+    planetResult.speed.latitude = planetParams[sigh++];
+    planetResult.speed.distance = planetParams[sigh++];
 
-    iflag = SEFLG_SPEED;
-
-    while (TRUE) {
-        printf("\nDate (d.m.y) ?");
-        gets(sdate);
-
-         /* stop if a period . is entered */
-        if (*sdate == '.')
-            return OK;
-
-        if (sscanf (sdate, "%d%*c%d%*c%d", &jday, &jmon, &jyear) < 1) exit(1);
-
-         /*
-         * we have day, month and year and convert to Julian day number
-         */
-
-        tjd_ut = swe_julday(jyear, jmon, jday, jut, SE_GREG_CAL);
-
-         /*
-         * compute Ephemeris time from Universal time by adding delta_t
-         * not required for Swisseph versions smaller than 1.60
-         */
-
-        te = tjd_ut + swe_deltat(tjd_ut);
-
-        printf("date: %02d.%02d.%d at 0:00 Universal time\n", jday, jmon, jyear);
-        printf("planet \tlongitude\tlatitude\tdistance\tspeed long.\n");
-
-         /*
-         * a loop over all planets
-         */
-
-        for (p = SE_SUN; p <= SE_CHIRON; p++) {
-            if (p == SE_EARTH) continue;
-
-             /*
-             * do the coordinate calculation for this planet p
-             */
-
-            iflgret = swe_calc_ut(tjd_ut, p, iflag, x2, serr);
-
-             /* if there is a problem, a negative value is returned and an
-             * error message is in serr.
-             */
-
-            if (iflgret < 0)
-                 printf("error: %s\n", serr);
-            /*
-            * get the name of the planet p
-            */
-
-            swe_get_planet_name(p, snam);
-
-            /*
-            * print the coordinates
-            */
-
-            printf("%10s\t%11.7f\t%10.7f\t%10.7f\t%10.7f\n", snam, x2[0], x2[1], x2[2], x2[3]);
-        }
-    }
-
-    return OK;
+    return &planetResult;
 }
