4d46a1a95aa8c6dce82afce7b78900bc672425e1
Shawn McMannis <skmcmannis@gmail.com>
2021-01-12 14:52:41 -0800

    Removed dead code

--- lib/components/ffi.dart
+++ lib/components/ffi.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:ephemeris/ephemeris.dart';
 
 void ffiInit() {
@@ -8,27 +7,14 @@ void ffiInit() {
 bool ffiCurrentStatus() {
   final now = new DateTime.now().toUtc();
   print('Using time: ${now}');
-  final pi = getPlanetInfo(SE_MERCURY, now.year, now.month, now.day, now.hour + now.minute/60);
+  final pi = getPlanetInfo(
+      SE_MERCURY, now.year, now.month, now.day, now.hour + now.minute / 60);
   //final pi = getPlanetInfo(SE_MERCURY, 2021, 1, 30, 7 + 51/60 + 8);  <-- stationing retrograde
-  print('${pi.position.longitude}, ${pi.position.latitude}, ${pi.position.distance} / ${pi.speed.longitude}, ${pi.speed.latitude}, ${pi.speed.distance}');
+  print('${pi.position.longitude}, ${pi.position.latitude}, ${pi.position
+      .distance} / ${pi.speed.longitude}, ${pi.speed.latitude}, ${pi.speed
+      .distance}');
 
   return pi.speed.longitude < 0;
-
-  //Generate random number between 1 and 10
-  var rng = new Random();
-  var rand = rng.nextInt(10);
-  print(rand);
-
-  //Use the modulus of the random number to generate a random bool
-  var preStatus = rand % 2;
-  print(preStatus);
-
-  //Return the bool
-  if(preStatus == 1) {
-    return true;
-  } else {
-    return false;
-  }
 }
 
 DateTime ffiNextChange() {
