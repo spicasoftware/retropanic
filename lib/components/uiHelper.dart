5dfd9978bb2a726c4ab4caac4cc629dfa78761ba
Shawn McMannis <skmcmannis@gmail.com>
2021-01-31 16:01:37 -0800

    Main UI work

--- lib/components/uiHelper.dart
+++ lib/components/uiHelper.dart
@@ -1,43 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:retropanic/components/ffi.dart';
 
 bool isDarkMode () {
   var brightness = SchedulerBinding.instance.window.platformBrightness;
   return brightness == Brightness.dark;
 }
 
-String mainUIIcon (status) {
-  if (status && !isDarkMode()) {
+String mainUIIcon (status, isDarkMode) {
+  if (status && !isDarkMode) {
     return 'assets/images/rp_retro_light.png';
-  } else if (status && isDarkMode()){
+  } else if (status && isDarkMode){
     return 'assets/images/rp_retro_dark.png';
-  } else if (!status && !isDarkMode()) {
+  } else if (!status && !isDarkMode) {
     return 'assets/images/rp_direct_light.png';
   } else {
     return 'assets/images/rp_direct_dark.png';
   }
 }
 
-String mainUIText (status) {
+String statusText (status) {
   if (status) {
-    return 'OMG PANIC!';
+    return 'Mercury is retrograde';
   } else {
-    return 'Chill, man...';
+    return 'Mercury is not retrograde';
   }
 }
 
-Color mainUITextColor (status) {
+String statusSubText (status) {
   if (status) {
-    return Color(0xFFFFD80C);
+    return 'Ends';
   } else {
-    return Color(0xFFF46F01);
+    return 'Begins';
   }
 }
 
-Color mainUIBackgroundColor (status) {
-  if (status) {
-    return Color(0xFF060606);
+Color mainUITextColor (isDarkMode) {
+  print('Next ${ffiNextMercuryChange()}');
+  print('Countdown ${ffiNextMercuryChange().millisecondsSinceEpoch + 1000 * 30}');
+  print('Current ${DateTime.now().millisecondsSinceEpoch + 1000 * 30}');
+  if (!isDarkMode) {
+    return Colors.black;
   } else {
     return Color(0xFFFBF8C6);
+  }
+}
+
+Color mainUIBackgroundColor (isDarkMode) {
+  if (!isDarkMode) {
+    return Color(0xFFFBF8C6);
+  } else {
+    return Colors.black;
   }
 }
